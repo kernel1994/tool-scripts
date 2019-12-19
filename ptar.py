c1c6971b756e57a15e15c80354f459eaef567fed
kernel1994 <almostlover@outlook.com>
2019-12-19 13:48:58 +0800

    update ptar.py
    A: -d, --delete to delete original files, dirs or .tar.gz file
    A: -n, --name to specific .tar.gz name

--- ptar.py
+++ ptar.py
@@ -1,74 +1,97 @@
 #!/path/to/anaconda3/bin/python
 # Compress files and dirs to .tar.gz, or uncompress .tar.gz file
 # TODO:
-# 1. -d to delete original files, dirs or .tar.gz file
-# 2. --name to specific .tar.gz name
+# 1. Decompression like Extract Here (Smart) in Bandizip
+#    case 1. When there is only one file in the archive, it is extracted to the current location.
+#    case 2. When all files in the archive are bundled in a single folder, they are extracted to the current location.
+#    case 3. In the other cases, your file(s) is/are extracted to the (archive-name) folder.
+import argparse
 import os
 import sys
 import time
-from typing import List
 
 TAR_SUFFIX = '.tar.gz'
 TIME_STAMP = time.strftime('%Y%m%d%H%M%S', time.localtime())
 TMP_NAME = 'tmp'
 
 
-def tarc(arg_files: List[str]):
+def tarc(args: argparse.Namespace):
     """
     Compress to .tar.gz
     """
-    n_arg_files = len(arg_files)
+    n_arg_files = len(args.files)
 
     if n_arg_files == 1:
-        tar_file_name = arg_files[0]
+        if args.name:
+            tar_file_name = args.name
+        else:
+            tar_file_name = args.files[0]
 
         if tar_file_name.endswith('/'):
             tar_file_name = tar_file_name[:-1]
 
-        tar_file = tar_file_name + '.' + TIME_STAMP + TAR_SUFFIX
     elif n_arg_files > 1:
-        tar_file = TMP_NAME + '.' + TIME_STAMP + TAR_SUFFIX
+        if args.name:
+            tar_file_name = args.name
+        else:
+            tar_file_name = TMP_NAME
+
     else:
         print('ERROR: unknown error!')
-        print(f'args: {arg_files} with len {n_arg_files}')
+        print(f'args: [{args.files}] with len {n_arg_files}')
         exit(-1)
 
-    arg_files_str = ' '.join(arg_files)
+    tar_file = tar_file_name + '.' + TIME_STAMP + TAR_SUFFIX
+    arg_files_str = ' '.join(args.files)
 
     cmd = f'tar -czvf {tar_file} {arg_files_str}'
     print(f'* run command: {cmd}\n')
     os.system(cmd)
 
+    if args.delete:
+        cmd = f'rm -r {arg_files_str}'
+        print(f'* run command: {cmd}\n')
+        os.system(cmd)
 
-def tarx(arg_files: List[str]):
+
+def tarx(args: argparse.Namespace):
     """
     Uncompress from .tar.gz
     """
-    tar_file = arg_files[0]
+    tar_file = args.files[0]
 
-    cmd = f'tar -xzvf {tar_file}'
+    if args.name:
+        if not os.path.exists(args.name):
+            cmd = f'mkdir {args.name}'
+            print(f'* run command: {cmd}\n')
+            os.system(cmd)
+        cmd = f'tar -xzvf {tar_file} -C {args.name}'
+
+    else:
+        cmd = f'tar -xzvf {tar_file}'
     print(f'* run command: {cmd}\n')
     os.system(cmd)
 
-
-def print_usage():
-    print('ERROR: not enough files or dirs to be compressed or uncompressed!')
-    print('Usage:')
-    print('Compress: ptar <file_or_dir_list>')
-    print('Uncompress: ptar <one_tar_file>.tar.gz')
+    if args.delete:
+        cmd = f'rm {tar_file}'
+        print(f'* run command: {cmd}\n')
+        os.system(cmd)
 
 
 def main():
-    arg_files = sys.argv[1:]  # sys.argv[0] is commond name
-    n_arg_files = len(arg_files)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='+',
+                        help='files or dirs to be compressed or a .tar.gz file to be uncompressed')
+    parser.add_argument('-n', '--name',
+                        help='specific .tar.gz file name')
+    parser.add_argument('-d', '--delete', action='store_true', default=False,
+                        help='delete original files')
+    args = parser.parse_args()
 
-    if n_arg_files == 0:
-        print_usage()
-        exit(-1)
-    elif n_arg_files == 1 and arg_files[0].endswith(TAR_SUFFIX):
-        tarx(arg_files)
+    if len(args.files) == 1 and args.files[0].endswith(TAR_SUFFIX):
+        tarx(args)
     else:
-        tarc(arg_files)
+        tarc(args)
 
 
 if __name__ == "__main__":
